cd6a149542c8efabc9bdac17c50840cf64b21d61
unknown <princy200>
2023-02-03 14:11:54 +0530

    sdhjgf

---
+++ src/app/candidate/candidate.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CanLoginComponent } from './can-login/can-login.component';
+
+
+
+@NgModule({
+  declarations: [
+    CanLoginComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CandidateModule { }
