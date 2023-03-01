1a4cd55437488637d6682bdb2d3bbe9abe57fa49
unknown <princy200>
2023-03-01 19:16:21 +0530

    dfsfds

--- src/app/candidate/updateprofile/updateprofile.component.ts
+++ src/app/candidate/updateprofile/updateprofile.component.ts
@@ -11,7 +11,7 @@ import { CanditateService } from '../canditate.service';
 })
 export class UpdateprofileComponent implements OnInit {
   qualification: any;
-  isSubmitted:any=false;
+  isSubmitted: any = false;
   profileForm: any = this.fb.group({
     image: new FormControl(''),
     keyskill: new FormControl(null, [Validators.required]),
@@ -23,8 +23,10 @@ export class UpdateprofileComponent implements OnInit {
     locationCurrent: new FormControl('', Validators.required),
     locationNative: new FormControl('', Validators.required),
     noticeperiod: new FormControl('', Validators.required),
-    currentSkill: new FormControl('', Validators.required),
-    preferredSkill: new FormControl('', Validators.required),
+    currentSkill: new FormControl(null, Validators.required),
+    currentbox: new FormControl(''),
+    preferredSkill: new FormControl(null, Validators.required),
+    prefredBox: new FormControl(''),
     gender: new FormControl('', Validators.required),
     maritalStatus: new FormControl('', Validators.required),
     relocate: new FormControl('', Validators.required),
@@ -58,7 +60,7 @@ export class UpdateprofileComponent implements OnInit {
     const datePipe = formatDate(new Date(), 'yyyy-MM-dd', 'en-IN')
     const time = formatDate(new Date(), 'hh:mm', 'en-IN')
     this.now = datePipe
-  console.log(this.profileForm.get('languages')?.valid,"validators")
+    console.log(this.profileForm.get('languages')?.valid, "validators")
 
   }
   getKeyskills(value: any) {
@@ -66,6 +68,7 @@ export class UpdateprofileComponent implements OnInit {
       this.keySkill = res;
     })
   }
+
   getAlldata() {
     this.candidateService.viewDetails().subscribe((res: any) => {
       this.viewAll = res.user;
@@ -92,9 +95,9 @@ export class UpdateprofileComponent implements OnInit {
         // languages: this.viewAll[0].keyskill
       })
       this.getLang = this.viewAll[0].languages;
-      if(this.viewAll[0].experienceYear){
-        this.profileForm.get('currentctc').setErrors({'incorrect':true})
-      }else{
+      if (this.viewAll[0].experienceYear) {
+        this.profileForm.get('currentctc').setErrors({ 'incorrect': true })
+      } else {
         this.profileForm.get('currentctc').setErrors(null)
       }
       this.viewAll[0].languages.forEach((element: any) => {
@@ -129,36 +132,101 @@ export class UpdateprofileComponent implements OnInit {
       if (value[value.length - 1] != null && value[value.length - 1] != '') {
         this.getKeyskills(value[value.length - 1])
         console.log("well working")
-      }else{
+      } else {
         console.log("working")
-        console.log(this.profileForm.get('keyskill')?.setErrors({'incorrect':true}),"bjdfjdbfjdfb")
+        console.log(this.profileForm.get('keyskill')?.setErrors({ 'incorrect': true }), "bjdfjdbfjdfb")
       }
     }
     this.profileForm.get('keyskill')?.setValue(value)
 
+  }
+  isShow = false;
+  displayPrefered(data: any) {
+    let val = data.target.value.split(",");
+    if (data.target.value) {
+      this.isShow = true
+      console.log(this.isShow,"isndj")
+    } else {
+      this.isShow = false
+      console.log(this.isShow,"isndjsbjkbjbjkb")
+    }
+    if (val.length != 0) {
+      if (val[val.length - 1] != null && val[val.length - 1] != '') {
+        this.getAllpreferd(val[val.length - 1])
+      } else {
+        // console.log("working")
+        // console.log(this.profileForm.get('prefredBox')?.setErrors({'incorrect':true}),"bjdfjdbfjdfb")
+      }
+    }
+    this.profileForm.get('preferredSkill')?.setValue(val)
+  }
+  checkPrefered(data:any,preSkill:any){
+    this.isShow = false;
+    let index: any = this.profileForm.get('preferredSkill')?.value;
+    if (index.length != 0) {
+      let value = index.splice([index.length - 1], 1);
+      index.push(preSkill)
+      this.profileForm.get('preferredSkill')?.setValue(index)
+      let search: any = index.toString() + ","
+      this.profileForm.get('prefredBox')?.setValue(search);
+    }
+  }
+  prferedSkill:any=[];
+  getAllpreferd(value:any){
+    this.candidateService.get_prefered(value).subscribe((res: any) => {
+    this.prferedSkill=res;
+    console.log(this.prferedSkill,'dfjdngjdgn')
+    })
+  }
+  displaycurent(val:any){
+    console.log("working")
+    let value = val.target.value.split(",");
+    if (val.target.value) {
+      this.iscurrent = true
+    } else {
+      this.iscurrent = false
+    }
+    if (value.length != 0) {
+      if (value[value.length - 1] != null && value[value.length - 1] != '') {
+        this.getAllpreferd(value[value.length - 1])
+      } else {
+        // console.log("working")
+        // console.log(this.profileForm.get('prefredBox')?.setErrors({'incorrect':true}),"bjdfjdbfjdfb")
+      }
+    }
+    this.profileForm.get('currentSkill')?.setValue(value)
+  }
+  iscurrent=false;
+  checkcurrent(val:any,currentskill:any){
+    this.iscurrent = false;
+    let index: any = this.profileForm.get('currentSkill')?.value;
+    if (index.length != 0) {
+      let value = index.splice([index.length - 1], 1);
+      index.push(currentskill)
+      this.profileForm.get('currentSkill')?.setValue(index)
+      let search: any = index.toString() + ","
+      this.profileForm.get('currentbox')?.setValue(search);
+    }
   }
   // push skills
 
   checkSkill(event: any, skill: any) {
     this.isDisplay = false;
     let index: any = this.profileForm.get('keyskill')?.value;
-    console.log(index.length, "wORKINGDKSDK")
     if (index.length != 0) {
-      console.log("workinf")
       let value = index.splice([index.length - 1], 1);
       index.push(skill)
-
-      this.profileForm.get('search')?.setValue(index)
+      this.profileForm.get('keyskill')?.setValue(index)
       let search: any = index.toString() + ","
       this.profileForm.get('searchbox')?.setValue(search);
     }
   }
   languageskill: any = [];
-  selectlanguages:any=[];
+  selectlanguages: any = [];
   insLang(val: any) {
     if (val.target.checked) {
       const data = this.profileForm.get('languages').push(this.fb.group({
-        lang: new FormControl(val.target.value,Validators.required),
+        lang: new FormControl(val.target.value, Validators.required),
         know: this.fb.array([])
       }));
     } else {
@@ -167,7 +235,7 @@ export class UpdateprofileComponent implements OnInit {
         this.languages?.removeAt(index)
       }
     }
-    console.log(this.profileForm.get('languages')?.invalid,"vailss")
+    console.log(this.profileForm.get('languages')?.invalid, "vailss")
   }
   get languages() {
     return this.profileForm.get('languages') as FormArray;
@@ -185,29 +253,30 @@ export class UpdateprofileComponent implements OnInit {
     language.get('kown')?.setValue(Known)
   }
   updateprofile() {
-    this.isSubmitted=true
+    this.isSubmitted = true
     const formData = new FormData();
     formData.append('image', this.selectImg1);
-    console.log(this.profileForm)
-    if (this.profileForm.valid) {
-      if (this.userId.tab == "0" || this.userId.id) {
-        this.candidateService.educationDetail(this.profileForm.value).subscribe((res: any) => {
-          if (this.userId.id) {
-            this.router.navigate(['/viewprofile'])
-          } else {
-            this.router.navigate(['/getAllprofile'])
-          }
-          this.candidateService.imageUpload(res.user._id, formData).subscribe((res: any) => {
-          })
-        })
-      } else {
-        this.candidateService.updateProfile(this.profileForm.value).subscribe((res: any) => {
-          this.candidateService.imageUpload(res.user._id, formData).subscribe((res: any) => {
-          })
-          this.router.navigate(['/can-edu'])
-        })
-      }
-    }
+    console.log(this.profileForm.value, "values")
+    console.log(this.profileForm.valid, "values")
+    // if (this.profileForm.valid) {
+    //   if (this.userId.tab == "0" || this.userId.id) {
+    //     this.candidateService.educationDetail(this.profileForm.value).subscribe((res: any) => {
+    //       if (this.userId.id) {
+    //         this.router.navigate(['/viewprofile'])
+    //       } else {
+    //         this.router.navigate(['/getAllprofile'])
+    //       }
+    //       this.candidateService.imageUpload(res.user._id, formData).subscribe((res: any) => {
+    //       })
+    //     })
+    //   } else {
+    //     this.candidateService.updateProfile(this.profileForm.value).subscribe((res: any) => {
+    //       this.candidateService.imageUpload(res.user._id, formData).subscribe((res: any) => {
+    //       })
+    //       this.router.navigate(['/can-edu'])
+    //     })
+    //   }
+    // }
 
   }
   checkeLang(val: any) {
@@ -228,13 +297,13 @@ export class UpdateprofileComponent implements OnInit {
     }
 
   }
-  expreience(val:any){
-   if(val.target.value == 0){
-    this.profileForm.get('currentctc').setErrors(null)
-    console.log(this.profileForm.get('experienceYear')?.value,this.profileForm.get('currentctc').setErrors(null))
-   }else{
-    console.log(this.profileForm.get('experienceYear')?.value,this.profileForm.get('currentctc').setErrors({'incorrect':true}))
-    this.profileForm.get('currentctc').setErrors({'incorrect':true})
-   }
+  expreience(val: any) {
+    if (val.target.value == 0) {
+      this.profileForm.get('currentctc').setErrors(null)
+      console.log(this.profileForm.get('experienceYear')?.value, this.profileForm.get('currentctc').setErrors(null))
+    } else {
+      console.log(this.profileForm.get('experienceYear')?.value, this.profileForm.get('currentctc').setErrors({ 'incorrect': true }))
+      this.profileForm.get('currentctc').setErrors({ 'incorrect': true })
+    }
   }
 }
